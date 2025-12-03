accf91db582904f557b5f212a260f51592f64d9f
bad4iz <developer@bad4iz.ru>
2025-12-03 20:22:36 +0400

    flash + surround + убрал кривое форматирование

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,6 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
+--
 -- Conform.nvim - modern formatting plugin
 
 ---@type LazySpec
--- lua/plugins/leap-nvim.lua
+++ lua/plugins/leap-nvim.lua
@@ -1,4 +1,7 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 return {
+
   {
     "ggandor/leap.nvim",
     keys = {
